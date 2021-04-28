ab0f1ede1cb26eb3f2532e0da3f3a51856f3f550
GalinaIs <denis@isyanov.com>
2021-04-28 13:53:33 +0300

    Add method guessNumber in ComputerMakeNumberImpl.

--- src/main/java/ru/myproject/computer/ComputerMakeNumberImpl.java
+++ src/main/java/ru/myproject/computer/ComputerMakeNumberImpl.java
@@ -2,10 +2,21 @@ package ru.myproject.computer;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class ComputerMakeNumberImpl implements ComputerMakeNumber {
     @Override
     public void tryGuessNumber(int countDigits) {
+        String guessedNumber = guessNumber(countDigits);
+    }
 
+    String guessNumber(int countDigits) {
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < countDigits; i++) {
+            stringBuilder.append(random.nextInt(10));
+        }
+        return stringBuilder.toString();
     }
 }
